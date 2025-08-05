d0d1f5bee4f0688e1002ac37bfb4e8b6242f1be2
rayulu <dasaradharam109@gmail.com>
2025-08-05 19:03:39 +0530

    code updated

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,12 +15,10 @@ export default function Home() {
   return <main className = "bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
     <section className = "main-section">
-      <div className = "page-heading">
+      <div className = "page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
-    </section>
-
 
     {resumes.length > 0 && (
         <div className = "resumes-section">
@@ -31,6 +29,7 @@ export default function Home() {
           ))}
         </div>
     )}
+    </section>
 
 
 
